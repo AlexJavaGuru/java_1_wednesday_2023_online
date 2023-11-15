02f4a05f4d003f9ccfed379d5c2f4c9ea6545832
AlexJavaGuru <55928462+AlexJavaGuru@users.noreply.github.com>
2023-11-15 21:51:27 +0200

    Lesson 1: Variables

---
+++ src/main/java/teacher/lesson_2/lessoncode/Variables.java
@@ -0,0 +1,18 @@
+package teacher.lesson_2.lessoncode;
+
+public class Variables {
+
+    public static void main(String[] args) {
+        String name = "Alex From Java Guru";
+
+        int age = 10;
+        double weight = 10.5;
+        char symbol = 'Y';
+
+        System.out.println("int variable = " + age);
+        System.out.println("symbol is " + symbol);
+        System.out.println("My Name is " + name);
+        System.out.println("Weight is " + weight);
+
+    }
+}
