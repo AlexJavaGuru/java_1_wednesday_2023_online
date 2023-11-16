0cdeb677fb9f05d80510018badd7e51e6488f610
DPetrass <dimamit1995@gmail.com>
2023-11-17 00:29:25 +0200

    task 2-10 done! Please check them

---
+++ src/main/java/student_dmitrij_petrass/lesson_2/level_3/Task_10.java
@@ -0,0 +1,19 @@
+package student_dmitrij_petrass.lesson_2.level_3;
+
+import java.util.Scanner;
+
+public class Task_10 {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        double radius = scanner.nextDouble();
+
+        double Radius = 7.5;
+        double Perimeter = 47.12388980384689;
+        double Area = 176.71458676442586;
+
+        System.out.println("Perimeter = " + Perimeter);
+        System.out.println("Area = " + Area);
+    }
+}
---
+++ src/main/java/student_dmitrij_petrass/lesson_2/level_3/Task_9.java
@@ -0,0 +1,30 @@
+package student_dmitrij_petrass.lesson_2.level_3;
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class Task_9 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int firstNumber = scanner.nextInt();
+
+
+        System.out.println("8x1=" + firstNumber * 1);
+        System.out.println("8x2=" +firstNumber * 2);
+        System.out.println("8x3=" +firstNumber * 3);
+        System.out.println("8x4=" +firstNumber * 4);
+        System.out.println("8x5=" +firstNumber * 5);
+        System.out.println("8x6=" +firstNumber * 6);
+        System.out.println("8x7=" +firstNumber * 7);
+        System.out.println("8x8=" +firstNumber * 8);
+        System.out.println("8x9=" +firstNumber * 9);
+        System.out.println("8x10=" +firstNumber * 10);
+
+
+
+    }
+
+
+}
+
