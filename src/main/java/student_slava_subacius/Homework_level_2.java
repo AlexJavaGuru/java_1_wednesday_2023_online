bde0941fb9b71919ed72a321e3af81a5f0b2c113
SlavaJava1 <150361254+SlavaJava1@users.noreply.github.com>
2023-11-16 09:43:43 +0200

    Homework 2

---
+++ src/main/java/student_slava_subacius/Homework_level_2.java
@@ -0,0 +1,18 @@
+package student_slava_subacius;
+
+import java.util.Scanner;
+
+public class Homework_level_2 {
+    public static void main(String[] args) {
+        double numA = 6.2;
+        double numB = 2.2;
+        System.out.println(numA + (double)numB);
+        System.out.println(numA - (double)numB);
+        System.out.println(numA / (double)numB);
+        System.out.println(numA * (double)numB);
+
+        Scanner scanner = new Scanner(System.in);
+        double fistDoubleNumber = scanner.nextDouble();
+
+    }
+}
