c7b2c581f8a2d1299b472d01d3aeec1bd07f186e
AlexJavaGuru <55928462+AlexJavaGuru@users.noreply.github.com>
2023-11-17 22:10:22 +0200

    AL Lesson 2; Task9 + Task10

---
+++ src/main/java/student_alex_ivanov/lesson_2/Task10.java
@@ -0,0 +1,9 @@
+package student_alex_ivanov.lesson_2;
+
+public class Task10 {
+
+    public static void main(String[] args) {
+        //Тут я работаю и делаю домашнее задание.
+    }
+
+}
---
+++ src/main/java/student_alex_ivanov/lesson_2/Task9.java
@@ -0,0 +1,8 @@
+package student_alex_ivanov.lesson_2;
+
+public class Task9 {
+
+    public static void main(String[] args) {
+        //Тут я работаю и делаю домашнее задание.
+    }
+}
