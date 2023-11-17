70c22b48faa63d3285438bd1e51dcb09e5422713
SlavaJava1 <150361254+SlavaJava1@users.noreply.github.com>
2023-11-17 20:43:40 +0200

    Homework 7

---
+++ src/main/java/lesson_2/class_book.java
@@ -0,0 +1,17 @@
+package lesson_2;
+
+public class class_book {
+    public static void main(String[] args) {
+
+
+        String author = "Slava";
+        int page = 100;
+        String title = "Test Book";
+
+        System.out.println("Name is " + author);
+        System.out.println("int variable = " + page);
+        System.out.println("Name is the book : " + title);
+
+      }
+
+}
