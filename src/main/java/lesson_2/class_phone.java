3a7e81fd88fdb486e810ba66cfdce338081209e3
SlavaJava1 <150361254+SlavaJava1@users.noreply.github.com>
2023-11-19 18:08:59 +0200

    class_phone + Task_5

---
+++ src/main/java/lesson_2/class_phone.java
@@ -0,0 +1,16 @@
+package lesson_2;
+
+public class class_phone {
+    public static void main(String[] args) {
+        String name = ("class phone ") ;
+        System.out.println(name);
+
+
+
+    }
+
+
+
+
+
+}
