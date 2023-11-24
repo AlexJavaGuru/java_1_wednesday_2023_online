2fd6eca54eb49af062d92c36cace8e2eb61ee113
SlavaJava1 <150361254+SlavaJava1@users.noreply.github.com>
2023-11-24 09:27:31 +0200

    slava lesson 3

---
+++ src/main/java/student_slava_subacius/class_Robot.java
@@ -0,0 +1,8 @@
+package student_slava_subacius;
+
+public class class_Robot {
+    public static void main(String[] args) {
+        demoRobot robot = new demoRobot();
+        robot.sayHello();
+    }
+}
---
+++ src/main/java/student_slava_subacius/demoRobot.java
@@ -0,0 +1,11 @@
+package student_slava_subacius;
+
+public class demoRobot {
+    public demoRobot() {
+
+    }
+
+    public void sayHello() {
+        System.out.println("Hello!");
+    }
+}
