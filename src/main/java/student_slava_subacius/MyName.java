d3adf1725039791ae928f65e63c85dfda0dae7d1
SlavaJava1 <150361254+SlavaJava1@users.noreply.github.com>
2023-11-27 22:30:42 +0200

    slava lesson 3_level2

---
+++ src/main/java/student_slava_subacius/MyName.java
@@ -0,0 +1,10 @@
+package student_slava_subacius;
+
+public class MyName {
+    public static void main(String[] args) {
+        String myName = "Slava";
+        System.out.println(myName);
+
+
+    }
+}
