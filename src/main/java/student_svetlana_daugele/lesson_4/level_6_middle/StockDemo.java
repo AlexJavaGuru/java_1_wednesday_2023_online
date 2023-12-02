d26fa4daa4d990496c682078b96f21d1e3da988c
svetlanadaugele <svetlana.daugele@vip.com.lv>
2023-12-02 21:07:36 +0200

    Homework for Lesson 4 - Stock and StockDemo

---
+++ src/main/java/student_svetlana_daugele/lesson_4/level_6_middle/StockDemo.java
@@ -0,0 +1,17 @@
+package student_svetlana_daugele.lesson_4.level_6_middle;
+
+class StockDemo {
+    public static void main(String[] args) {
+    Stock google = new Stock("GOOG", 10);
+
+    String priceInformation = google.getPriceInformation();
+System.out.println(priceInformation);
+
+google.updatedPrice(15);
+google.updatedPrice(7);
+google.updatedPrice(14);
+
+    priceInformation = google.getPriceInformation();
+System.out.println(priceInformation);
+}
+}
