624e412248f16e7d2a8f14d371451c8f569a5c7e
svetlanadaugele <svetlana.daugele@vip.com.lv>
2023-12-05 07:49:25 +0200

    Lesson 4 - Stock Test

--- src/main/java/student_svetlana_daugele/lesson_4/level_6_middle/StockTest.java
+++ src/main/java/student_svetlana_daugele/lesson_4/level_6_middle/StockTest.java
@@ -3,22 +3,23 @@ package student_svetlana_daugele.lesson_4.level_6_middle;
 class StockTest {
     public static void main(String[] args) {
         StockTest stockTest = new StockTest();
-        stockTest.actualPriceTest1();//new price = 10
-        stockTest.actualPriceTest2();//new price = 12
-        stockTest.actualPriceTest3();//new price = 5
-        stockTest.actualPriceTest4();//new price = 7
-        stockTest.actualPriceTest5();//new price = 99
-        stockTest.actualPriceTest6();//new price = 77
+        stockTest.actualPriceTest(10,10 );//new price = 10
+        stockTest.actualPriceTest(12,12 );
+        stockTest.actualPriceTest(5,5 );
+        stockTest.actualPriceTest(7,7 );
+        stockTest.actualPriceTest(99,99 );
+        stockTest.actualPriceTest(77,77 );
+
+
         stockTest.maxPriceTest1(); //Изменение цены акции: 999  = Максимальная цена за указанный переуд: 999
         stockTest.maxPriceTest2(); //Изменение цены акции: 999 -> 1  = Максимальная цена за указанный переуд: 999
         stockTest.maxPriceTest3();//Изменение цены акции: 999 -> 2 -> 1000 = Максимальная цена за указанный переуд: 1000
         stockTest.maxPriceTest4();//Изменение цены акции: 999 -> 2 -> 1000 -> 8 = Максимальная цена за указанный переуд: 1000
         stockTest.maxPriceTest5();// Изменение цены акции: 9 -> 27 = Максимальная цена за указанный переуд: 27
     }
-    public void actualPriceTest1() {
-        int expectedActualPrice = 10;
+    public void actualPriceTest(int expectedActualPrice, int newPrice) {
         Stock stock = new Stock("GOOGLE", 10);
-        int realResult = stock.updatedPrice(10);
+        int realResult = stock.updatedPrice(newPrice);
         if (realResult == expectedActualPrice) {
             System.out.println("Updated price test = OK");
         } else {
@@ -26,76 +27,26 @@ class StockTest {
         }
     }
 
-    public void actualPriceTest2() {
-        int expectedActualPrice = 12;
-        Stock stock = new Stock("GOOGLE", 10);
-        int realResult = stock.updatedPrice(12);
-        if (realResult == expectedActualPrice) {
-            System.out.println("Updated price test = OK");
-        } else {
-            System.out.println("Updated price test = FAIL");
-        }
-    }
-
-    public void actualPriceTest3() {
-        int expectedActualPrice = 5;
-        Stock stock = new Stock("GOOGLE", 10);
-        int realResult = stock.updatedPrice(5);
-        if (realResult == expectedActualPrice) {
-            System.out.println("Updated price test = OK");
-        } else {
-            System.out.println("Updated price test = FAIL");
-        }
-    }
-
-    public void actualPriceTest4() {
-        int expectedActualPrice = 7;
-        Stock stock = new Stock("GOOGLE", 10);
-        int realResult = stock.updatedPrice(7);
-        if (realResult == expectedActualPrice) {
-            System.out.println("Updated price test = OK");
-        } else {
-            System.out.println("Updated price test = FAIL");
-        }
-    }
-
-    public void actualPriceTest5() {
-        int expectedActualPrice = 99;
-        Stock stock = new Stock("GOOGLE", 10);
-        int realResult = stock.updatedPrice(99);
-        if (realResult == expectedActualPrice) {
-            System.out.println("Updated price test = OK");
-        } else {
-            System.out.println("Updated price test = FAIL");
-        }
-    }
-
-    public void actualPriceTest6() {
-        int expectedActualPrice = 77;
-        Stock stock = new Stock("GOOGLE", 10);
-        int realResult = stock.updatedPrice(77);
-        if (realResult == expectedActualPrice) {
-            System.out.println("Updated price test = OK");
-        } else {
-            System.out.println("Updated price test = FAIL");
-        }
-    }
     public void maxPriceTest1() {
-        int expectedMaxprice = 999;
+        int expectedMaxPrice = 999;
         Stock stock = new Stock("GOOGLE", 10);
+        stock.updatedPrice(999);
+        stock.getMaxPrice(999);
         int realResult = stock.getMaxPrice(999);
-        if (realResult == expectedMaxprice) {
+        if (realResult == expectedMaxPrice) {
             System.out.println("Max price test = OK");
         } else {
             System.out.println("Max price test = FAIL");
         }
     }
     public void maxPriceTest2() {
-        int expectedMaxprice = 999;
+        int expectedMaxPrice = 999;
         Stock stock = new Stock("GOOGLE", 10);
-        stock.getMaxPrice(999);
+        stock.updatedPrice(999);
+        stock.updatedPrice(1);
+        stock.getMaxPrice(1);
         int realResult = stock.getMaxPrice(1);
-        if (realResult == expectedMaxprice) {
+        if (realResult == expectedMaxPrice) {
             System.out.println("Max price test = OK");
         } else {
             System.out.println("Max price test = FAIL");
@@ -106,6 +57,7 @@ class StockTest {
         Stock stock = new Stock("GOOGLE", 10);
         stock.getMaxPrice(999);
         stock.getMaxPrice(2);
+        stock.getMaxPrice(1000);
         int realResult = stock.getMaxPrice(1000);
         if (realResult == expectedMaxprice) {
             System.out.println("Max price test = OK");
