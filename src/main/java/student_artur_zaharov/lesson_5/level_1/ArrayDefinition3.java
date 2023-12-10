3189052d903e5ba5e3ddabfdaaed101069b8936e
LVArturZ <artur.zaharov.tt@gmail.com>
2023-12-10 20:22:24 +0200

    Artur Zaharov _ leson 5

---
+++ src/main/java/student_artur_zaharov/lesson_5/level_1/ArrayDefinition3.java
@@ -0,0 +1,10 @@
+package student_artur_zaharov.lesson_5.level_1;
+
+class ArrayDefinition3 {
+
+    public static void main(String[] args) {
+        int[] numbers = new int[10];
+    }
+
+}
+
---
+++ src/main/java/student_artur_zaharov/lesson_5/level_3/Task_16.java
@@ -0,0 +1,12 @@
+package student_artur_zaharov.lesson_5.level_3;
+
+public class Task_16 {
+
+    public static void main(String[] args) {
+
+        for (int i =0; i < 10; i++) {
+            System.out.println(i);
+        }
+    }
+
+}
