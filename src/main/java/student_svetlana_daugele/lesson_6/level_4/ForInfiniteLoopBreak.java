4f873c1c32fab478949ac7274cfafc300e2c51b0
svetlanadaugele <svetlana.daugele@vip.com.lv>
2023-12-17 00:22:28 +0200

    homework lesson 6 part 1

---
+++ src/main/java/student_svetlana_daugele/lesson_6/level_4/ForInfiniteLoopBreak.java
@@ -0,0 +1,10 @@
+package student_svetlana_daugele.lesson_6.level_4;
+
+class ForInfiniteLoopBreak {
+    public static void main(String[] args) {
+        for (;;) {
+            System.out.println("Hello, infinity!");
+            break;
+        }
+    }
+}
---
+++ src/main/java/student_svetlana_daugele/lesson_6/level_4/WhileInfiniteLoopBreak.java
@@ -0,0 +1,10 @@
+package student_svetlana_daugele.lesson_6.level_4;
+
+public class WhileInfiniteLoopBreak {
+    public static void main(String[] args) {
+        while (1 == 1) {
+            System.out.print("Infinite Loop");
+            break;
+        }
+    }
+}
