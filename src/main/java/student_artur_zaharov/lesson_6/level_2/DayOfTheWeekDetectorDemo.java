a1693740f38ce820e848c163975546532ca90b3b
LVArturZ <artur.zaharov.tt@gmail.com>
2023-12-24 00:06:35 +0200

    Arturs Zaharovs _ lesson 6

--- src/main/java/student_artur_zaharov/lesson_6/level_2/DayOfTheWeekDetectorDemo.java
+++ src/main/java/student_artur_zaharov/lesson_6/level_2/DayOfTheWeekDetectorDemo.java
@@ -1,21 +1,15 @@
-//package student_artur_zaharov.lesson_6.level_2;
-//
-//public class DayOfTheWeekDetectorDemo {
-//
-//    public static void main(String[] args) {
-//        DayOfTheWeekDetector detector = new DayOfTheWeekDetector();
-//        int dayNumber = detector.getDayNumberFromUser();
-//
-//        switch (firstNumber) {
-//            case 1 -> System.out.println("Monday");
-//            case 2 -> System.out.println("Tuesday");
-//            case 3 -> System.out.println("Wednesday");
-//            case 4 -> System.out.println("Thursday");
-//            case 5 -> System.out.println("Friday");
-//            case 6 -> System.out.println("Saturday");
-//            case 7 -> System.out.println("Sunday");
-//            default -> System.out.println("Not correct day number");
-//        }
-//    }
-//
-//}
+package student_artur_zaharov.lesson_6.level_2;
+
+public class DayOfTheWeekDetectorDemo {
+
+    public static void main(String[] args) {
+        DayOfTheWeekDetector detector = new DayOfTheWeekDetector();
+        int dayNumber = detector.getDayNumberFromUser();
+        String dayOfWeek = detector.findDayOfTheWeek(dayNumber);
+        System.out.println(dayOfWeek);
+
+
+
+    }
+
+}
