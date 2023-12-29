8da9068336a3669090645c2daf24d37f99c5d126
SlavaJava1 <150361254+SlavaJava1@users.noreply.github.com>
2023-12-29 13:52:42 +0200

    slava_lesson_8_2

---
+++ src/main/java/student_slava_subacius/lesson_8_2/Child.java
@@ -0,0 +1,18 @@
+package student_slava_subacius.lesson_8_2;
+
+
+class Mother {
+
+}
+
+class Father {
+
+}
+class Child implements Parent {
+    @Override
+    public void motherMetod() {
+    }
+    @Override
+    public void fatherMetod() {
+    }
+}
---
+++ src/main/java/student_slava_subacius/lesson_8_2/Porsche911Demo.java
@@ -0,0 +1,15 @@
+package student_slava_subacius.lesson_8_2;
+
+public class Porsche911Demo {
+    public static void main(String[] args) {
+        Porsche911 myPorsche = new Porsche911(2023, 200);
+        System.out.println(myPorsche.toString());
+    }
+}
+
+
+
+
+
+
+
