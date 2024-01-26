33b62e97915bdcfab55d0b5cfff6b28d64ffe1cd
svetlanadaugele <svetlana.daugele@vip.com.lv>
2024-01-26 09:07:24 +0200

    Homework Lesson 11

---
+++ src/main/java/student_svetlana_daugele/lesson_11/level_3/SuperClassConstructorInvoker.java
@@ -0,0 +1,9 @@
+package student_svetlana_daugele.lesson_11.level_3;
+
+public class SuperClassConstructorInvoker {
+    public SuperClassConstructorInvoker() {
+        super();
+        System.out.println(1);
+        System.out.println(2);
+    }
+}
