d8b9ea894a579d467b3081c67fb2a08e1f46145e
svetlanadaugele <svetlana.daugele@vip.com.lv>
2024-02-03 06:32:36 +0200

    Homework Lesson 12

---
+++ src/main/java/student_svetlana_daugele/lesson_12/level_2/AccessDeniedException.java
@@ -0,0 +1,10 @@
+package student_svetlana_daugele.lesson_12.level_2;
+
+ class AccessDeniedException extends Exception {
+
+
+  public AccessDeniedException(String message) {
+   super(message);
+  }
+
+ }
